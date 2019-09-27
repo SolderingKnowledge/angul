f00a309b38b3445d75f95752083385a43b5222f1
Aikol Tashbolot <aikoltashbolot@aikol-streamline-macpro.local>
2019-09-27 15:57:55 -0700

    string interpolation

--- src/app/backend/backend.component.ts
+++ src/app/backend/backend.component.ts
@@ -3,8 +3,18 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-backend',
     templateUrl: './backend.component.html'
+    // template: `<app-backend></app-backend>`
 })
 
 export class BackendComponent {
+    id: number = 5;
+    status: string = "live";
 
+    getStatus(){
+        return ` got status: ${this.status}`
+    }
 }
+
+
+
+
